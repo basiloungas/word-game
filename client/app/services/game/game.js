1d4d052d062e25048d27e7a1b2ec5e4d2f461f17
Bill Trikalinos <billtrik@gmail.com>
2017-06-04 20:19:59 +0200

    Update GameService to provide functionality to play a Game

--- client/app/services/game/game.js
+++ client/app/services/game/game.js
@@ -1,6 +1,8 @@
 class GameService {
   baseUrl = 'https://api.backendless.com/82263891-8692-3866-FFA6-96A77B10E300/2ACCCE44-EF77-5B8A-FF36-9AF9F03DF000/data/words';
 
+  secondsPerGame = 40;
+
   constructor($http, $q) {
     this.$q = $q;
     this.$http = $http;
@@ -9,8 +11,13 @@ class GameService {
   }
 
   resetState() {
+    this.gameStarted = false;
     this.words = null;
     this.activeWord = null;
+    this.activeWordIndex = -1;
+    this.secondsLeft = this.secondsPerGame;
+
+    clearTimeout(this.timerEnd);
   }
 
   initialize() {
@@ -28,8 +35,74 @@ class GameService {
       url: this.baseUrl
     };
 
+    // TODO: abort XHR
     return this.$http(httpOptions)
       .then(this.parseQueryResult);
+  }
+
+  start(options) {
+    const {
+      onTimerEnd,
+      onTimerTick,
+    } = options;
+
+    this.gameStarted = true;
+    this.selectNextWord();
+
+    setTimeout(() => {
+      this.tick(onTimerEnd, onTimerTick);
+    }, 0);
+  }
+
+  stop() {
+    // ???
+  }
+
+  tick(onTimerEnd, onTimerTick) {
+    onTimerTick(this.secondsLeft);
+    return;
+
+    this.timerEnd = setTimeout(() => {
+      this.secondsLeft -= 1;
+
+      if (this.secondsLeft === 0) {
+        onTimerEnd();
+        // this.stop();
+      } else {
+        this.tick(onTimerEnd, onTimerTick);
+      }
+    }, 1000)
+  }
+
+  onTimerEnd(callback) {
+  }
+
+  getSolvedWordsCount() {
+    return this.activeWordIndex;
+  }
+
+  getTotalScore() {
+    let sum = 0;
+
+    for (let i = 0; i < this.activeWordIndex; i++) {
+      sum += this.words[i].currentScore;
+    }
+
+    return sum;
+  }
+
+  getActiveWord() {
+    return this.activeWord;
+  }
+
+  reducePointForActiveWord() {
+    this.activeWord.currentScore = Math.max(this.activeWord.currentScore -1, 0);
+  }
+
+  selectNextWord() {
+    // TODO: handle scenario when words run out
+    this.activeWordIndex = this.activeWordIndex + 1;
+    this.activeWord = this.words[this.activeWordIndex];
   }
 
   parseQueryResult(result) {
